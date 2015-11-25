51723d8fe1cc93b188d58a5f54b6f49ea71da360
mlnorman <michael@mlnorman.com>
2015-11-25 04:37:06 -0500

    chp5 ex1

---
+++ Chp5/Exercise_05_01.java
@@ -0,0 +1,39 @@
+/*
+    Write a program that reads an unspecified number of integers, determines how many
+    positive and negative values have been read, and computes the total and average of
+    the input values (not counting zeros). Your program ends with the input 0. Display
+    the average as a floating-point number.
+*/
+
+import java.util.Scanner;
+
+public class Exercise_05_01{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter a series of integers seperated by a space (ex. 1 2 -5 3 0), the input ends if it is 0: ");
+        int val = in.nextInt();
+        int total = 0;
+        int numOfPos = 0;
+        int numOfNeg = 0;
+        double avg = 0;
+        int count = 0;
+
+        while (val != 0){
+            total += val;
+            
+            if (val > 0)
+                numOfPos += 1;
+            else
+                numOfNeg += 1;
+
+            count += 1;
+
+            val = in.nextInt();
+        }
+
+        avg = total / (double) count;
+        // need to finish the output
+        System.out.printf("total: %d average: %.2f%n", total, avg);
+    }
+}
