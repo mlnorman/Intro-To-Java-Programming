36b757043e83e9404438327838bdb0e4a2e1bb11
Atul Narayan <73711346+atulnarayan16@users.noreply.github.com>
2020-12-27 03:11:35 +0530

    Update Exercise_02_15.java

--- Chp2/Exercise_02_15.java
+++ Chp2/Exercise_02_15.java
@@ -3,7 +3,7 @@
     (Geometry: distance of two points) Write a program that prompts the user to enter
     two points (x1, y1) and (x2, y2) and displays their distance between them.
     The formula for computing the distance is 2(x2 - x1)2 + (y2 - y1)2. Note that
-    you can use Math.pow(a, 0.5) to compute 2a.
+    you can use Math.pow(a, 0.5) to compute √a.
 
 
 */
